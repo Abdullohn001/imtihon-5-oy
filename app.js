8203f641c52188389d6552f08c8314e7ca852cf2
abdullohn001 <muhammadaligulomov41@gmail.com>
2024-06-11 19:32:04 +0500

    redy

---
+++ app.js
@@ -0,0 +1,71 @@
+const temp = document.querySelector("template");
+const ul = document.querySelector("ul");
+
+const updateUI = (products) => {
+  ul.innerHTML = "";
+  products.forEach((product) => {
+    const clone = temp.content.cloneNode(true);
+    console.log(product);
+
+    const h3 = clone.querySelector("h3");
+    const a = clone.querySelector("a");
+    const img = clone.querySelector("img");
+    const para = clone.querySelector("p");
+    const span = clone.querySelector("span");
+    const heart = clone.querySelector("#heart");
+    const cartNumber = document.getElementById("cart-number");
+    const cards = clone.getElementById("cards");
+    const numberOfDiscount = clone.getElementById("numberOfDiscount");
+    const starRating = clone.getElementById("star-rating");
+
+    let counter = 0;
+
+    if (product.discountPercentage) {
+      para.style = `
+          text-decoration: line-through;
+          opacity: 0.5;
+      `;
+      span.textContent =
+        (
+          product.price -
+          (product.price * product.discountPercentage) / 100
+        ).toFixed(2) + " $";
+    }
+
+    img.src = product.thumbnail;
+
+    starRating.textContent = product.rating + "/5";
+
+    h3.textContent = product.title;
+    para.textContent = "Old price : " + product.price + " $";
+
+    a.href = `../about/about.html?product=${product.id}`;
+
+    numberOfDiscount.textContent = product.discountPercentage + "% ";
+
+    heart.addEventListener("click", () => {
+      heart.classList.toggle("red");
+    });
+
+    const bosish = () => {
+      a.click();
+    };
+    cards.addEventListener("click", bosish);
+
+    ul.appendChild(clone);
+  });
+};
+
+const API = "https://dummyjson.com/products?limit=50";
+
+const getData = async (url) => {
+  const overlay = document.getElementById("overlay");
+  overlay.classList.remove("hidden");
+  const request = await fetch(url);
+  const data = await request.json();
+  overlay.classList.add("hidden");
+  return data;
+};
+getData(API)
+  .then((data) => updateUI(data.products))
+  .catch((error) => console.log(error));
